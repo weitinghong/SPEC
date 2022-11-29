2271cc56a61557c95cb1bdf9fb4ab6becd8d02f6
med287 <95094045+med287@users.noreply.github.com>
2022-11-29 16:34:46 -0600

    Add files via upload

---
+++ Spec_Maps&Plots.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import geopandas as gpd
+
+
+# In[5]:
+
+
+get_ipython().system('pip install pygeos')
+
+
+# In[7]:
+
+
+gpd.options.use_pygeos=True
+
+
+# In[20]:
+
+
+gdf_isochrone = gpd.read_file('/Users/mariedemple/Downloads/spec_score.shp')
+gdf_isochrone
+
+
+# In[19]:
+
+
+gdf.crs
+
+
+# In[21]:
+
+
+gdf_parcel = gpd.read_file('/Users/mariedemple/Downloads/spec_score.shp')
+gdf_parcel
+
+
+# In[23]:
+
+
+geometry = gpd.points_from_xy(gdf_parcel.x, gdf_parcel.y, crs="EPSG:4326")
+geometry
+
+
+# In[25]:
+
+
+gdf_parcel['geometry_p'] = geometry
+
+
+# In[26]:
+
+
+gdf_parcel.info()
+
+
+# In[34]:
+
+
+gdf_parcel.drop(columns=['geometry'], inplace=True)
+
+
+# In[42]:
+
+
+gdf_parcel.rename(columns={'geometry_p':'geometry'}, inplace=True)
+
+
+# In[46]:
+
+
+get_ipython().system('pip install streamlit')
+
+
+# In[47]:
+
+
+import streamlit as st
+
+
+# In[44]:
+
+
+st.gdf_parcel.plot()
+
+
+# In[43]:
+
+
+gdf_parcel.info()
+
+
+# In[ ]:
+
+
+#f, ax = plt.subplots(1, figsize=(15,15))
+#gdf_parcel.plot('basezone', ax=ax, legend=True)
+#land_gdf.plot(ax=ax, alpha=0.1)
+#plt.show()
+
